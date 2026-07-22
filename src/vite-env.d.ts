0e01bbf198fbed0ad175d9b3502c9cf9e31ec93e
Totito-STY <totitosty@gmail.com>
2026-07-22 03:16:45 -0400

    Delete src directory

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-declare module '*.avif' {
-  const src: string;
-  export default src;
-}
-declare module '*.webp' {
-  const src: string;
-  export default src;
-}
